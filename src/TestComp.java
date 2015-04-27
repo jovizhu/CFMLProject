2d7027c96dd8c087349806632e7a8ca20fe23cd0
James Crisman <jamie@longest.voyage>
2015-04-27 15:17:25 -0500

    comments and some cleanup

--- src/TestComp.java
+++ src/TestComp.java
@@ -27,62 +27,77 @@ import apa.*;
 import apa.impl.DefaultCPU;
 
 public class TestComp {
-
+	final static String OWL_FILE = "facility2.owl";
+	final static String BPMN_FILE = "testingServices.bpmn2";
 	public static void main(String[] args) throws Exception {
 		
 		OWLOntologyManager owlm = OWLManager.createOWLOntologyManager();
-		File owlFile = new File("facility2.owl");
+		File owlFile = new File(OWL_FILE);
 		OWLOntology owl = owlm.loadOntologyFromOntologyDocument(owlFile);
 		SomeFacility x = new SomeFacility(owl);
 		
-		//System.out.println(x.getAllFacilityInstances());
-		//System.out.println(x.getAllServiceInstances());
+		//put all the services into an array
 		Object[] s;
 		s = x.getAllServiceInstances().toArray();
+		
+		//put all the micro servicese into another array
 		Object[] ms;
 		ms = x.getAllMServiceInstances().toArray();
-		/*
-		for(int i = 0; i < s.length; i++){
-			System.out.println("Service: " + ((Service) s[i]).getOwlIndividual().getIRI().getFragment());
-			Object[] f = ((Service)s[i]).getPerformAt().toArray();
-			System.out.println("Takes Place At:");
-			for(int z = 0; z < f.length; z++){
-				System.out.println("  "+((Facility)f[z]).getOwlIndividual().getIRI().getFragment());
-				Object[] c = ((Facility)f[z]).getHasCFD().toArray();
-				System.out.println("    CFDs:");
-				for(int cc = 0; cc < c.length; cc++){
-					System.out.println("      " +((CFD)c[cc]).getOwlIndividual().getIRI().getFragment() );
-					System.out.println("      " + ((CFD)c[cc]).calculateCFD());
-				}
-			}
-		}*/
 		
-		ArrayList<ProcessNode> services = readBPMNSpec("testingServices.bpmn2");
-		ProcessNode ss = null; // our starting node
+		//process BPMN and put the results into the services list
+		ArrayList<ProcessNode> services = readBPMNSpec(BPMN_FILE);
+		
+		ProcessNode ss = null; // our starting service
+		
+		//look at each service and find our starting service
 		for(ProcessNode a: services){
-			//System.out.println(a);
+			//Might want to find a better way to select the starting node
 			if(a.getID().compareTo("StartEvent_1") == 0){
 				ss = a; //start node is a
 			}
 		}
+		
+		//if we don't find the starting node
 		if(ss == null){
 			throw(new Exception("start event not set/detected."));
 		}
-		//int CFL = 0;
-		boolean prologMode = true;
+		
+			
+		//workflow object to manage which service to walk through
 		Workflow wf = new Workflow(ss);
+		
+		//set the current service to the next wf service
 		ss = wf.next();
+		
+		//bunch of strings to build the prolog code.
+		//there's so many of them because prolog has to have it's facts next to eachother.
+		//and the way we iterate it won't get all of them in one go.
+		// for example we get:
+		// service(orderprocessing).
+		// hasscf(scf, orderprocessing).
+		// ...
+		// in one loop, but we need
+		// service(orderprocessing).
+		// service(scheduling).
+		// ...etc
+		
 		String prologCode = "", servicesCode = "",microsCode="", 
 			    usesCode = "", producesCode = "", instanceOf = "", mfCFL = "",
 			    scfCode = "", madeFromCode = "", madeFromQuantity = "",
 			    performAtCode = "", cfdCode= "", mfListString = "";
+		
+		//what a product is made from
 		ArrayList<String> madeFromList = new ArrayList<>();
+		
+		//is this process a micro service?
 		boolean isMicro = false;
-		//System.out.println(ss);
-		//System.out.println(wf.getQueue());
+		
+		//loop services
 		do{
-			//System.out.println("!!-- " + ss.getName());
 			Service thisService = null;
+			
+			
+			//figure out what this service is and if it's a microservice or not
 			for(int i = 0; i < s.length; i++){
 				if(ss.getName().compareTo(((Service) s[i]).getOwlIndividual().getIRI().getFragment()) == 0){
 					thisService = (Service)s[i];
@@ -100,26 +115,26 @@ public class TestComp {
 					}
 				}
 			}
+			//if for some reason we still don't know what service it is, just continue to the next one
 			if(thisService == null){
 				ss = wf.next();
 				continue;
-				//System.out.println("Workflow '" + ss.getName()+  " was not found in ontology." );
-				//return;
 			}
-			if(!prologMode)
-				System.out.println("----------------\nService: " + thisService.getOwlIndividual().getIRI().getFragment()+ "\n----------------");
+			
+			//get all the info about this service
 			Object[] f = thisService.getPerformAt().toArray();
 			Object[] scf = thisService.getHasSCF().toArray();
 			Object[] uses = thisService.getUses().toArray();
 			Object[] p = thisService.getProduces().toArray();
-			String service = thisService.getOwlIndividual().getIRI().getFragment();
-			if(prologMode){
-				if(!isMicro){
-					servicesCode += "service("+service+").\n";
-				}else{
-					microsCode += "microservice("+service+").\n";
-				}
+			String service = thisService.getOwlIndividual().getIRI().getFragment(); //service name
+			
+			//define service/microservice
+			if(!isMicro){
+				servicesCode += "service("+service+").\n";
+			}else{
+				microsCode += "microservice("+service+").\n";
 			}
+			//if it's a microservice find some CFL services
 			if(isMicro){
 				String cfl = "";
 				Map<OWLDataPropertyExpression, Set<OWLLiteral>> msprop = thisService.getOwlIndividual().getDataPropertyValues(thisService.getOwlOntology());
@@ -128,88 +143,55 @@ public class TestComp {
 					String property = msprop.keySet().toArray()[q].toString().split("#")[1].replaceAll("[^A-Za-z0-9]", "");
 					if(property.compareTo("CFLabel")==0){
 						cfl = msprop2.toArray()[q].toString().split("\"")[1];
-						//System.out.println("ind name: " + indName);
 					}else{
-						System.out.println("found unexpected property: " + property);
+						System.out.println("!found unexpected property: " + property);
 					}
 				}
 				mfCFL += "hasCFLabel("+service+", "+cfl+ ").\n";
 			}
+			
+			//if this service produces something, create the related 
 			if(p.length > 0){
 				madeFromList.clear();
 				String product = "";
-				if(!prologMode)
-					System.out.println("Produces: ");
+				//produces
 				for(int ri = 0; ri < p.length; ri++){
 					product = ((Computer)p[ri]).getOwlIndividual().getIRI().getFragment();
-					if(prologMode){
-						producesCode += "producedBy("+ product +", "+ service +").\n";
-					}else{
-						System.out.println("  #---------------\n   "+((Computer)p[ri]).getOwlIndividual().getIRI().getFragment());
-					}
+					producesCode += "producedBy("+ product +", "+ service +").\n";
 					Object[] pc = ((Computer)p[ri]).getMadeFrom().toArray();
-					if(!prologMode)
-						System.out.println("  # Made From");
 					for(int rr = 0; rr < pc.length; rr++){
-						//hope this works
-						// heads up... we'll have to rework this to be much nicer to get the quantity values.
-						// this will likely break if individuals' quantity, name properties are changed.
-						if(prologMode){
-							String material = ((Computer)pc[rr]).getOwlIndividual().getIRI().getFragment();
-							Map<OWLDataPropertyExpression, Set<OWLLiteral>> test = ((DefaultCPU)pc[rr]).getOwlIndividual().getDataPropertyValues(((DefaultCPU)pc[rr]).getOwlOntology());
-							String indName = "", quantity = "0", cfl = "";
-							Collection<Set<OWLLiteral>> test2 = test.values();
-							for(int q = 0; q < test.keySet().toArray().length; q++){
-								String property = test.keySet().toArray()[q].toString().split("#")[1].replaceAll("[^A-Za-z0-9]", "");
-								if(property.compareTo("name")==0){
-									indName = test2.toArray()[q].toString().split("\"")[1];
-									//System.out.println("ind name: " + indName);
-								}else if(property.compareTo("quantity")==0){
-									quantity = test2.toArray()[q].toString().split("\"")[1];
-									//System.out.println("quantity: " + 	quantity);
-								}else if(property.compareTo("CFLabel")==0){
-									cfl = test2.toArray()[q].toString().split("\"")[1];
-								}else{
-									System.out.println("found unexpected property: " + property);
-								}
-							}
-							madeFromQuantity += "hasQuantity("+indName+", "+quantity+").\n";
-							madeFromCode += "madeFrom("+material+", "+product+").\n" ;
-							madeFromList.add(material);
-							instanceOf += "instanceOf("+indName+", "+material+").\n";
-							//TODO: get mfCFL and link it to the individual 
-							mfCFL += "hasCFLabel("+ material + ", "+ cfl +").\n";
-						}else{
-							//System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~"+((Computer)pc[rr]).getOwlIndividual().asOWLNamedIndividual());
-							Map<OWLDataPropertyExpression, Set<OWLLiteral>> test = ((DefaultCPU)pc[rr]).getOwlIndividual().getDataPropertyValues(((DefaultCPU)pc[rr]).getOwlOntology());
-							String indName = "", quantity = "0", cfl="";
-							String material = ((Computer)pc[rr]).getOwlIndividual().getIRI().getFragment();
-							Collection<Set<OWLLiteral>> test2 = test.values();
-							for(int q = 0; q < test.keySet().toArray().length; q++){
-								String property = test.keySet().toArray()[q].toString().split("#")[1].replaceAll("[^A-Za-z0-9]", "");
-								if(property.compareTo("name")==0){
-									indName = test2.toArray()[q].toString().split("\"")[1];
-									//System.out.println("ind name: " + indName);
-								}else if(property.compareTo("quantity")==0){
-									quantity = test2.toArray()[q].toString().split("\"")[1];
-									//System.out.println("quantity: " + 	quantity);
-								}else if(property.compareTo("CFLabel")==0){
-									cfl = test2.toArray()[q].toString().split("\"")[1];
-								}else{
-									System.out.println("found unexpected property: " + property);
-								}
-							}
+						// 
+						//	This section may cause problems due to it looking for specific data property names "name", "quantity", and "cflabel" 
+						// 	
+						//
+						//
+						String material = ((Computer)pc[rr]).getOwlIndividual().getIRI().getFragment();
+						Map<OWLDataPropertyExpression, Set<OWLLiteral>> test = ((DefaultCPU)pc[rr]).getOwlIndividual().getDataPropertyValues(((DefaultCPU)pc[rr]).getOwlOntology());
+						String indName = "", quantity = "0", cfl = "";
+						Collection<Set<OWLLiteral>> test2 = test.values();
+						for(int q = 0; q < test.keySet().toArray().length; q++){
 							
-							//
-							madeFromQuantity += "hasQuantity("+indName+", "+quantity+").\n";
-							instanceOf += "instanceOf("+indName+", "+product+").\n";
-							mfCFL += "hasCFLabel("+ material + ", "+ cfl +").\n";
-							System.out.println("  #!! q("+ quantity + ") name:" + indName + " individual: " + material +" CFL:" + cfl );
+							//all the strange splits are because the owl stores the data strangely
+							
+							String property = test.keySet().toArray()[q].toString().split("#")[1].replaceAll("[^A-Za-z0-9]", "");
+							if(property.toLowerCase().compareTo("name")==0){
+								indName = test2.toArray()[q].toString().split("\"")[1];
+							}else if(property.toLowerCase().compareTo("quantity")==0){
+								quantity = test2.toArray()[q].toString().split("\"")[1];
+							}else if(property.toLowerCase().compareTo("cflabel")==0){
+								cfl = test2.toArray()[q].toString().split("\"")[1];
+							}else{
+								System.out.println("found unexpected property: " + property);
+							}
 						}
-						//CFL += ((Computere)pc[rr]).getCFValue();
+						
+						//save properties to prolog code
+						madeFromQuantity += "hasQuantity("+indName+", "+quantity+").\n";
+						madeFromCode += "madeFrom("+material+", "+product+").\n" ;
+						madeFromList.add(material);
+						instanceOf += "instanceOf("+indName+", "+material+").\n"; 
+						mfCFL += "hasCFLabel("+ material + ", "+ cfl +").\n";
 					}
-					if(!prologMode)
-						System.out.println("  #---------------");
 				}
 				String templist = "";
 				for(int y =0; y < madeFromList.size(); y++){
@@ -221,100 +203,65 @@ public class TestComp {
 				}
 				mfListString += "mflist( "+product+", [" + templist + "]).\n";
 			}
+			//scf
 			if(scf.length > 0){
-				if(!prologMode)
-					System.out.println("Has SCF: ");
-				
 				for(int ri = 0; ri < scf.length; ri++){
 					String someSCF = ((SCF)scf[ri]).getOwlIndividual().getIRI().getFragment();
-					if(prologMode){
-						scfCode += "hasSCF("+someSCF+", "+ service +").\n";
-					}else{
-						System.out.println("  "+someSCF);
-					}
-					//CFL += ((SCFe)scf[ri]).getSCFValue();
+					scfCode += "hasSCF("+someSCF+", "+ service +").\n";
 				}
 			}
+			//uses
 			if(uses.length > 0){
-				if(!prologMode)
-					System.out.println("Uses: ");
 				for(int ri = 0; ri < uses.length; ri++){
 					String equipment = ((Equipment)uses[ri]).getOwlIndividual().getIRI().getFragment();
-					if(prologMode){
-						usesCode += "uses("+equipment+", "+service+").\n";
-					}else{
-						System.out.println("  "+equipment);
-					}
+					usesCode += "uses("+equipment+", "+service+").\n";
 				}
 			}
-			if(!prologMode)
-				System.out.println("Takes Place At:");
+			//location
 			for(int z = 0; z < f.length; z++){
 				String facility = ((Facility)f[z]).getOwlIndividual().getIRI().getFragment();
-				if(prologMode){
-					performAtCode += "performedAt("+facility+", "+service+").\n" ;
-				}else{
-					System.out.println("  "+facility);
-				}
+				performAtCode += "performedAt("+facility+", "+service+").\n" ;
 				Object[] c = ((Facility)f[z]).getHasCFD().toArray();
-				if(!prologMode)
-					System.out.println("    CFDs:");
 				for(int cc = 0; cc < c.length; cc++){
 					String cfd = ((CFD)c[cc]).getOwlIndividual().getIRI().getFragment();
-					if(prologMode){
-						cfdCode += "hasCFD("+cfd+", "+facility+"). \n";
-					}else{
-						System.out.println("      " + cfd);
-					}
-					//CFL += ((CFDe)c[cc]).getCFDValue();
-					//System.out.println("      " + ((CFD)c[cc]).calculateCFD());
+					cfdCode += "hasCFD("+cfd+", "+facility+"). \n";
 				}
 			}
 			ss = wf.next();
 		}while(wf.hasNext());
-		//System.out.println("Final CFL: " + CFL);
-		if(prologMode){
-			prologCode = servicesCode + microsCode + usesCode + scfCode + mfListString + madeFromCode + mfCFL + instanceOf + madeFromQuantity + performAtCode + cfdCode + producesCode;
-			System.out.println(prologCode.toLowerCase());
-			System.out.println("hasextendedquantity(X, Y) :- instanceof(A, X), hasquantity(A, Y).");
-			System.out.println("hosts(X,Y) :- performedat(Y,X).");
-			/*
-			 *  carbon(Obj, CF) :- hascflabel(Obj, CF).
-				carbon(Obj, CF) :- producedby(Obj, PS),
-									hascflabel(PS, CFps),
-									mflist(Obj, MFL), 
-									get_CF(MFL, CFmfl), 
-									CF is CFps + CFmfl.
-				
-				get_CF([], 0).
-				get_CF([FirstMFL|LastMFL], CFmfl) :-
-					hascflabel(FirstMFL, CFfirst),
-					hasextendedquantity(FirstMFL, Qfirst),
-					get_CF(LastMFL, CFLast),
-					CFmfl is CFfirst * Qfirst + CFLast.
-			 */
-			System.out.println("carbon(Obj, CF) :- hascflabel(Obj, CF).");
-			System.out.println("carbon(Obj, CF) :- producedby(Obj, PS),\n\thascflabel(PS, CFps),\n\tmflist(Obj, MFL),\n\tget_CF(MFL, CFmfl),\n\tCF is CFps + CFmfl.");
-			System.out.println("get_CF([], 0).");
-			System.out.println("get_CF([FirstMFL|LastMFL], CFmfl) :-\n\thascflabel(FirstMFL, CFfirst),\n\thasextendedquantity(FirstMFL, Qfirst),\n\tget_CF(LastMFL, CFLast),\n\tCFmfl is CFfirst * Qfirst + CFLast.");
-			
-		}
+		prologCode = servicesCode + microsCode + usesCode + scfCode + mfListString + madeFromCode + mfCFL + instanceOf + madeFromQuantity + performAtCode + cfdCode + producesCode;
+		
+		//can modify this portion to write directly to a file, but for now it's going to console
+		
+		System.out.println(prologCode.toLowerCase()); //to lowercase because prolog
+		
+		//these are just some simple rules to connect values
+		System.out.println("hasextendedquantity(X, Y) :- instanceof(A, X), hasquantity(A, Y).");
+		System.out.println("hosts(X,Y) :- performedat(Y,X).");
+		
+		//from the paper
+		System.out.println("carbon(Obj, CF) :- hascflabel(Obj, CF).");
+		System.out.println("carbon(Obj, CF) :- producedby(Obj, PS),\n\thascflabel(PS, CFps),\n\tmflist(Obj, MFL),\n\tget_CF(MFL, CFmfl),\n\tCF is CFps + CFmfl.");
+		System.out.println("get_CF([], 0).");
+		System.out.println("get_CF([FirstMFL|LastMFL], CFmfl) :-\n\thascflabel(FirstMFL, CFfirst),\n\thasextendedquantity(FirstMFL, Qfirst),\n\tget_CF(LastMFL, CFLast),\n\tCFmfl is CFfirst * Qfirst + CFLast.");
+		
 		
 		
 	}
 	
+	
+	// this parses our workflow from bpmn
 	public static ArrayList<ProcessNode> readBPMNSpec(String filename){
 		try{ //catch in case the file reader freaks out 
-			//long testTime = System.currentTimeMillis();
+			
 			File fXmlFile = new File(filename); //get the bpmn2 file
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance(); //build doc builder factory
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder(); // build the doc builder
 			Document doc = dBuilder.parse(fXmlFile); //parse the bpmn2 file into a doc
 			doc.getDocumentElement().normalize(); // normalize gets rid of junk characters I think
-			//testTime = System.currentTimeMillis()-testTime;
-			//System.out.println("setting up document time: " + testTime);
+			
+			
 			//removing tags you know won't be in the bpmn2 file will increase the loadtime.
-			//testTime = System.currentTimeMillis();
 			List<Node> nodes = searchDoc(doc,
 								new String[]{  			//these are the tags that it looks for in the xml
 								 "bpmn2:startEvent",		//!!! these are nodes. not connections
@@ -339,14 +286,12 @@ public class TestComp {
 								 "bpmn2:serviceTask",
 								 "bpmn2:userTask"
 								});
-			//testTime = System.currentTimeMillis()-testTime;
-			//System.out.println("grabbing relevent nodes: " + testTime);
+			
+			
 			// XML NODES => PROCESS NODES
-			//testTime = System.currentTimeMillis();
 			List<ProcessNode> pNodes = new ArrayList<>(); //process nodes
 			for(Node n: nodes){	//for all the xml nodes we found
 				Element e = (Element)n; //convert to an element so we can get the attributes
-				//System.out.println(e.getAttribute("name") +"   "+ e.getAttribute("id"));
 				pNodes.add( new ProcessNode(e.getAttribute("name"), e.getAttribute("id") )  ); //convert to process nodes 
 			}
 			
@@ -439,17 +384,6 @@ public class TestComp {
 					s.addNodeLink(t);				//make target a child of source
 				}
 			}
-			//testTime = System.currentTimeMillis()-testTime;
-			//System.out.println("connecting nodes: " + testTime);
-			//print out process nodes
-			/*for(ProcessNode a: pNodes){
-				if(a.linkCount == 0 && a.links.size() == 0){ //skip empty
-					System.out.println(a.getID()+ " :: "+a.getName() +" is a empty node that we're skipping");
-					continue;
-				}
-				System.out.println("--------------"+a.getName()+ "::" + a.getNum() +"----------------");
-				System.out.println(a); //shows process node and its children
-			}*/
 			
 			
 			return (ArrayList<ProcessNode>) pNodes;
@@ -459,6 +393,7 @@ public class TestComp {
 		return null;
 	}
 	
+	//just reads all the different types of tags returns the node
 	public static ArrayList<Node> searchDoc(Document doc, String[] searchList){
 		List<Node> ret = new ArrayList<>(); // create a list to return
 		NodeList nList = null; // nodelist that the document finds
